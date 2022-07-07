dbe4dbade5bfd4119ac361d521a34630ba0e651f
J34N-V4LJ34N <jeanmani2003@gmail.com>
2022-07-07 06:00:57 +0530

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       boxShadow: {
         header: ["0px 2px 24px rgba(0, 0, 0, 0.06)"],
         card: ["0px 4px 10px rgba(0, 0, 0, 0.1)"],
+        contact: ["0px 4px 24px rgba(0, 0, 0, 0.12)"],
       },
       gridTemplateColumns: {
         main: ["15rem 1fr"],
